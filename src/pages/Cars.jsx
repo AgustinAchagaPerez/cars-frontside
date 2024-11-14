0fb9cc6f7cd723ba160c66c4f6955327146e64aa
agustin.achagaperez <achagaperezagustin2015@gmail.com>
2024-11-14 16:54:21 -0300

    se agregó carpeta pages y archivo cars

---
+++ src/pages/Cars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CarList from '../components/CarList';
+
+const Cars = () => (
+  <div>
+    <h1>Autos en Alquiler</h1>
+    <CarList />
+  </div>
+);
+
+export default Cars;
